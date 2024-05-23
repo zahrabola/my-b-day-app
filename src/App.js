bc53f1c69e22de20ffd341b01c3f6cc8bca54f8a
zahrabola <95353397+zahrabola@users.noreply.github.com>
2024-05-23 18:15:09 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Data from "./Data";
+import List from "./List";
+import "./App.css";
 
 function App() {
+  const [people, setPeople] = useState(Data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <section className="container">
+          <h3>{people.length} birthday today</h3>
+
+          <List people={people} />
+          <button onClick={() => setPeople([])}>clear all</button>
+        </section>
+      </main>
     </div>
   );
 }
 
 export default App;
+//https://www.uidesigndaily.com/posts/sketch-birthdays-list-card-widget-day-1042
